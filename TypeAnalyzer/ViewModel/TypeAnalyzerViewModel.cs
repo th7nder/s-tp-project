6729a3038a664b5faccaa96cd4a03bc9dfaf764b
Konrad Stępniak <vesphy@gmail.com>
2019-11-15 21:55:10 +0100

    Basic window with TreeView and working filechooser

---
+++ TypeAnalyzer/ViewModel/TypeAnalyzerViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TypeAnalyzer.ViewModel
+{
+  class TypeAnalyzerViewModel : ViewModelBase
+  {
+    public TypeAnalyzerViewModel()
+    {
+      BrowseCommand = new RelayCommand(Browse);
+    }
+
+    public ICommand BrowseCommand { get; }
+    private String _pathVariable;
+    public string PathVariable
+    {
+      get => _pathVariable;
+      set
+      {
+        if (value != null && _pathVariable != value)
+        {
+          _pathVariable = value;
+          RaisePropertyChanged();
+        }
+      }
+    }
+ 
+    private void Browse()
+    {
+      PathVariable = GetPath();
+    }
+
+    private string GetPath()
+    {
+      OpenFileDialog openFileDialog = new OpenFileDialog();
+      openFileDialog.Multiselect = false;
+      if (openFileDialog.ShowDialog() == true)
+      {
+        return openFileDialog.FileName;
+      }
+
+      return null;
+    }
+  }
+}
