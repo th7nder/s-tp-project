7ce3613b6732b11d01b2184ac72901073514d82d
Konrad Stępniak <vesphy@gmail.com>
2019-11-15 22:28:52 +0100

    Model Project and TreeItemViewModel

---
+++ TypeAnalyzer.Model/PropertyMetadata.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeAnalyzer.Model
+{
+  public class PropertyMetadata
+  {
+    public string Name { get; set; }
+    public TypeMetadata TypeMetadata { get; set; }
+  }
+}
---
+++ TypeAnalyzer/ViewModel/TreeItemViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeAnalyzer.ViewModel
+{
+  public class TreeItemViewModel : ViewModelBase
+  {
+
+    public string Name { get; set; }
+    public ObservableCollection<TreeItemViewModel> Children { get; } = new ObservableCollection<TreeItemViewModel>() { null };
+    public bool TreeViewItemIsExpanded
+    {
+      get => m_IsExpanded;
+      set
+      {
+        m_IsExpanded = value;
+        if (m_WasBuilt)
+          return;
+        Children.Clear();
+        BuildMyself();
+        m_WasBuilt = true;
+        RaisePropertyChanged();
+      }
+    }
+
+    private bool m_WasBuilt = false;
+    private bool m_IsExpanded = false;
+    private static Random m_Random = new Random();
+    private void BuildMyself()
+    {
+      int _numberOfChildren = Math.Max(1, m_Random.Next(7));
+      for (int i = 0; i < _numberOfChildren; i++)
+        this.Children.Add(new TreeItemViewModel() { Name = $"sample{i}" });
+    }
+  }
+}
