bdbeb5a04fad4b323b740008a02fabf5973cd11e
Konrad Stępniak <vesphy@gmail.com>
2019-11-16 02:09:57 +0100

    Add Examples project to generate DLLs

---
+++ TypeAnalyzer.Examples/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeAnalyzer.Examples
+{
+    public class Class1
+    {
+      public Class1 Class { get; set; }
+    }
+}
