fc90ddf34dd25bfe81eb151ae0d05363f579a599
Konrad Stępniak <vesphy@gmail.com>
2019-11-17 11:01:23 +0100

    Change Save XML dialog

--- TypeAnalyzer/ViewModel/TypeAnalyzerViewModel.cs
+++ TypeAnalyzer/ViewModel/TypeAnalyzerViewModel.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Reflection;
-using System.Windows.Forms;
+using System.Windows;
 using System.Windows.Input;
 using TypeAnalyzer.Model;
 using TypeAnalyzer.Model.Serialization;
@@ -42,21 +40,6 @@ namespace TypeAnalyzer.ViewModel
       }
     }
 
-    private string _xmlSavePath;
-    public string XMLSavePath
-    {
-      get => _xmlSavePath;
-      set
-      {
-        if (value != null && _xmlSavePath != value)
-        {
-          _xmlSavePath = value;
-          RaisePropertyChanged();
-        }
-      }
-    }
-
-
     private AssemblyMetadata assemblyMetadata = null;
     private void Browse()
     {
@@ -71,32 +54,33 @@ namespace TypeAnalyzer.ViewModel
         return;
       }
 
-      XMLSavePath = GetDirectory();
-      if (XMLSavePath == null)
+      string savePath = GetSavePath();
+      if (savePath == null)
       {
         return;
       }
 
       XmlSerialization xmlSerialization = new XmlSerialization();
-      xmlSerialization.WriteFile(assemblyMetadata, $@"{XMLSavePath}/AssemblyMetadata.xml" );
+      xmlSerialization.WriteFile(assemblyMetadata, savePath);
 
-      MessageBox.Show("Succesfully serialized data", "Data serializaton", MessageBoxButtons.OK, MessageBoxIcon.Information);
+      MessageBox.Show($"Succesfully serialized data to {savePath}", "Data serializaton", MessageBoxButton.OK, MessageBoxImage.Information);
     }
 
-    private DialogResult ShowErrorBox(string message)
+    private MessageBoxResult ShowErrorBox(string message)
     {
-      return MessageBox.Show(message, "Error occured", MessageBoxButtons.OK, MessageBoxIcon.Error);
+      return MessageBox.Show(message, "Error occured", MessageBoxButton.OK, MessageBoxImage.Error);
     }
 
-    private string GetDirectory()
+    private string GetSavePath()
     {
-      using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+      SaveFileDialog saveFileDialog = new SaveFileDialog()
       {
-        DialogResult dialogResult = folderBrowserDialog.ShowDialog();
-        if (dialogResult == DialogResult.OK && !string.IsNullOrEmpty(folderBrowserDialog.SelectedPath))
-        {
-          return folderBrowserDialog.SelectedPath;
-        } 
+        Filter = "XML File (*.xml)|*.xml",
+      };
+
+      if (saveFileDialog.ShowDialog() == true)
+      {
+        return saveFileDialog.FileName;
       }
 
       return null;
@@ -104,7 +88,7 @@ namespace TypeAnalyzer.ViewModel
 
     private string GetPath()
     {
-      Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog
+      OpenFileDialog openFileDialog = new OpenFileDialog
       {
         Multiselect = false
       };
