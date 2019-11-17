5788b5c3bafbc1cb06c91d53c166faf0d67d6eca
Adikso <adam@zambrzycki.net>
2019-11-17 14:36:11 +0100

    Make BaseTypesViewModel based on TypesViewModel

--- TypeAnalyzer/ViewModel/TreeItemViewModels/BaseTypesViewModel.cs
+++ TypeAnalyzer/ViewModel/TreeItemViewModels/BaseTypesViewModel.cs
@@ -1,30 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using TypeAnalyzer.Model;
 
 namespace TypeAnalyzer.ViewModel.TreeItemViewModels
 {
-    class BaseTypesViewModel : TreeItemViewModel
+    class BaseTypesViewModel : TypesViewModel
     {
-        private readonly IEnumerable<TypeMetadata> _types;
-
-        public BaseTypesViewModel(IEnumerable<TypeMetadata> types)
-        {
-            Name = "Base types";
-            _types = types;
-        }
-
-        protected override void BuildMyself()
-        {
-            IEnumerable<TypeViewModel> types = from typeMetadata in _types
-                                               select new TypeViewModel(typeMetadata);
-            foreach (TypeViewModel type in types)
-            {
-                Children.Add(type);
-            }
-        }
+        public BaseTypesViewModel(IEnumerable<TypeMetadata> types) : base(types, "Base types") {}
     }
 }
---
+++ TypeAnalyzer/ViewModel/TreeItemViewModels/TypesViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using TypeAnalyzer.Model;
+
+namespace TypeAnalyzer.ViewModel.TreeItemViewModels
+{
+    public class TypesViewModel : TreeItemViewModel
+    {
+        private readonly IEnumerable<TypeMetadata> _types;
+
+        public TypesViewModel(IEnumerable<TypeMetadata> types, string name = "Types")
+        {
+            Name = name;
+            _types = types;
+        }
+
+        protected override void BuildMyself()
+        {
+            IEnumerable<TypeViewModel> types = from typeMetadata in _types
+                                               select new TypeViewModel(typeMetadata);
+            foreach (TypeViewModel type in types)
+            {
+                Children.Add(type);
+            }
+        }
+    }
+}
